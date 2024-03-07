b4f1af8656309c556024a69961e9ebbd2d61cb60
Axel <axelvasquez92@gmail.com>
2024-03-07 11:51:25 +0100

    falta

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+
+function myfuntion() {
+    
+
+    
+    const nombre = document.getElementById("nombre").textContent
+    const apellido = document.getElementById("apellido").textContent
+    const numero = document.getElementById("numero").textContent
+    const ninos = document.getElementById("ninos").textContent
+
+    let desayuno = document.getElementById("desayuno").textContent
+    let pension = document.getElementById("pension").textContent
+    let completo = document.getElementById("completo").textContent
+    
+    if(comida)
+    if (comida == true) {
+        alert("Solo desayuno")
+    }
+
+    else if (comida) {
+        alert("Media pensión")
+    }
+
+    else {
+        alert("Pensión completa")
+        return
+    } 
+
+}
+console.log(myfuntion);
+
